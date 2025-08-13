676054467e0cefc75cdb5eb6b61b4b8075f0055a
Fishy <87027056+phqshy@users.noreply.github.com>
2025-08-12 23:11:15 -0400

    Load .env before module initialization

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os
 import nest_asyncio
 from dotenv import load_dotenv
 
+load_dotenv()
+
 from bot import discord_bot
 from nationstates.api import api_coroutine
 from nationstates.foundings_sse import sse_coroutine
 
 nest_asyncio.apply()
-load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 
